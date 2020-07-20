13805d7b2b3dd6a1c24b481b507702bbc2b55ea7
Michael T. Mayers <michael@tweakoz.com>
2020-07-19 18:49:25 -0700

    ..

---
+++ build.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import os
+
+os.system("docker build "
+          "--build-arg PETA_VERSION=2020.1 "
+          "--build-arg PETA_RUN_FILE=petalinux-v2020.1-final-installer.run "
+          "-t petalinux:2020.1 .")
