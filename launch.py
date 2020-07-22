c3ff686cc8b2820f9b227ec1f5cffb47676b193c
Michael T. Mayers <michael@tweakoz.com>
2020-07-22 10:44:01 -0700

    fix XQuartz binding for docker

--- launch.py
+++ launch.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
-import os
+import os, platform
 
-os.system('docker run -ti --rm -e DISPLAY=$DISPLAY '
+DISPLAY=os.environ["DISPLAY"]
+
+osname = platform.system()
+
+if osname=="Darwin": 
+ os.system("xhost + 127.0.0.1") 
+ DISPLAY="host.docker.internal:0"
+
+
+os.system('docker run -ti --rm -e DISPLAY=%s '
           '--net="host" -v /tmp/.X11-unix:/tmp/.X11-unix '
           '-v $HOME/.Xauthority:/home/vivado/.Xauthority '
           '-v $HOME/Xilinx:/home/vivado/project '
           'petalinux:2020.1 '
-          '/bin/bash')
+          '/bin/bash' % DISPLAY )
