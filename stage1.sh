0898e86630355233ee84c135ddf252ee38471fbb
Michael T. Mayers <michael@tweakoz.com>
2020-08-08 23:14:00 -0700

    add yosys, icestorm

--- stage1.sh
+++ stage1.sh
@@ -4,7 +4,11 @@
 
 sed -i.bak s/archive.ubuntu.com/$UBUNTU_MIRROR/g /etc/apt/sources.list
 dpkg --add-architecture i386
+
+# for SpinalHDL # https://github.com/SpinalHDL/SpinalTemplateSbt
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
 apt-get update
+apt-get install -y -q software-properties-common
 apt-get install -y -q build-essential sudo vim tofrodos iproute2 gawk net-tools expect libncurses5-dev tftpd update-inetd libssl-dev flex bison
 apt-get install -y -q libselinux1 gnupg wget socat gcc-multilib libidn11 libsdl1.2-dev libglib2.0-dev lib32z1-dev zlib1g:i386
 apt-get install -y -q libgtk2.0-0 screen pax diffstat xvfb xterm texinfo gzip unzip cpio chrpath autoconf lsb-release
@@ -12,6 +16,11 @@ apt-get install -y -q libtool libtool-bin locales kmod git rsync bc u-boot-tools
 apt-get install -y -q gperf help2man screen tmux x11-xserver-utils
 apt-get install -y -q opencl-headers ocl-icd-opencl-dev ocl-icd-libopencl1
 apt-get install -y -q libboost-dev libboost-filesystem-dev libboost-program-options-dev uuid-dev dkms libprotoc-dev protobuf-compiler libxml2-dev libudev-dev
+apt-get install -y -q openjdk-8-jdk openocd libftdi-dev
+apt-get install -y -q libreadline-dev tcl tcl-dev clang
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+apt-get update
+apt-get install -y -q scala sbt
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
--- stage3.sh
+++ stage3.sh
@@ -10,6 +10,10 @@ wget https://raw.githubusercontent.com/enjoy-digital/litex/master/litex_setup.py
 chmod +x litex_setup.py
 sudo -u eda HOME=/home/eda ./litex_setup.py init install --user
 
+#Nmigen
+pip3 install nmigen
+
+
 #XilinxBoardStore
 cd /tmp
 git clone https://github.com/tweakoz/XilinxBoardStore
@@ -20,9 +24,41 @@ sudo cp -r XilinxBoardStore/boards/Digilent/nexys4 /opt/xilinx/Vivado/2020.1/dat
 sudo cp -r XilinxBoardStore/boards/Digilent/nexys_video /opt/xilinx/Vivado/2020.1/data/boards/board_files/
 sudo cp -r XilinxBoardStore/boards/Digilent/zedboard /opt/xilinx/Vivado/2020.1/data/boards/board_files/
 
+#yosys
+git clone https://github.com/cliffordwolf/yosys.git
+cd yosys
+git checkout 9a4f420b4b8285bd05181b6988c35ce45e3c979a
+make -j 8
+sudo make install
+
+#icestorm
+git clone https://github.com/YosysHQ/icestorm
+cd icestorm
+git checkout d12308775684cf43ab923227235b4ad43060015e
+make -j 8
+sudo make install
+
+git clone https://github.com/YosysHQ/SymbiYosys
+cd SymbiYosys
+#git checkout d12308775684cf43ab923227235b4ad43060015e
+sudo make install
+make test
+
 # cleanup
 rm -rf /tmp/*
 
 cd /home/eda/
 git clone https://github.com/tweakoz/Vitis-In-Depth-Tutorial
 git clone https://github.com/tweakoz/ultrazed_ev_cc_hello_pl
+git clone https://github.com/cliffordwolf/SimpleVOut
+git clone https://github.com/ucb-bar/chisel-tutorial
+git clone https://github.com/SpinalHDL/SpinalTemplateSbt
+
+#SpinalHDL
+cd /home/eda/SpinalTemplateSbt
+sbt run   # select "mylib.MyTopLevelVhdl" in the menu
+ls MyTopLevel.vhd
+
+#SpinalHDL
+cd /home/eda/chisel-tutorial/hello
+make
